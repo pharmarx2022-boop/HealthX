d30c31a68d58f2fc895e589078a8cbb4a055607b
PHARMA <p2218107@gmail.com>
2025-09-03 12:13:40 +0000

    why any button are not working

--- src/components/home/role-cards.tsx
+++ src/components/home/role-cards.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import Link from 'next/link';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
